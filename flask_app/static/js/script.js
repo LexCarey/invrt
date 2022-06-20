2c546198f20fea43b8c829b68e580590834fcdb0
lexCarey <lexcarey73@gmail.com>
2022-06-20 14:06:26 -0700

    Size Dynamically Changes

--- flask_app/static/js/script.js
+++ flask_app/static/js/script.js
@@ -2,20 +2,47 @@ main = document.getElementById("main_image");
 front = document.getElementById("Front");
 back = document.getElementById("Back");
 design = document.getElementById("Design");
+xsbutton = document.getElementById("xs");
+sbutton = document.getElementById("s");
+mbutton = document.getElementById("m");
+lbutton = document.getElementById("l");
+xlbutton = document.getElementById("xl");
+addcart = document.getElementById("AddCartButton");
 
-function changeView(element, img) {
+const changeView = (element, img) => {
     element.src = img;
 }
 
-function changeMain(element) {
+const changeMain = (element) => {
     main.src = element.src;
 }
 
-function changeColor(back_img, front_img, design_img){
+const changeColor = (xs, s, m, l, xl, back_img, front_img, design_img) =>{
     main.src = back_img;
     front.src = front_img;
     back.src = back_img;
     design.src = design_img;
+    xsbutton.onclick = function() {changeSize(xs)}
+    if (xsbutton.checked) {
+        changeSize(xs)
+    }
+    sbutton.onclick = function() {changeSize(s)}
+    if (sbutton.checked) {
+        changeSize(s)
+    }
+    mbutton.onclick = function() {changeSize(m)}
+    if (mbutton.checked) {
+        changeSize(m)
+    }
+    lbutton.onclick = function() {changeSize(l)}
+    if (lbutton.checked) {
+        changeSize(l)
+    }
+    xlbutton.onclick = function() {changeSize(xl)}
+    if (xlbutton.checked) {
+        changeSize(xl)
+    }
+
 }
 
 // function changeColor(pictures){
@@ -25,9 +52,25 @@ function changeColor(back_img, front_img, design_img){
 //     }
 // }
 
-function swapColor(productid, back, front){
+const swapColor = (productid, back, front) => {
     product = document.getElementById(productid);
     product.src = back;
     product.onmouseover = function() {changeView(this, front)}
     product.onmouseout = function() {changeView(this, back)}
+}
+
+const changeSize = (stock) => {
+    quantity = document.getElementById("quantity");
+    quantity.max = stock
+    if (stock>0) {
+        quantity.value = 1
+        quantity.min = 1
+        addcart.value = "Add To Cart"
+        addcart.disabled = false
+    } else {
+        quantity.value = 0
+        quantity.min = 0
+        addcart.value = "Sold Out"
+        addcart.disabled = true
+    }
 }
